4ebaff3800de9b6d2376be4f979e69c8a4348e37
Maisaferreiragit <maisaferreira.github@gmail.com>
2025-05-25 14:53:06 +0000

    Criei o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,10 +3,67 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
 
 int main() {
+
+    char estado;
+  char codigo [10] = "R01";
+  char cidade [20];
+  int populacao;
+  float area;
+  float pib;
+  int pontos; 
+
+  //Cadastro das Cartas:
+  //Carta 1:
+
+  printf("Carta 1: \n");
+  printf("Digite uma letra de 'A' a 'Z': \n");
+  scanf("%s", &estado);
+
+  printf("Digite a letra do estado seguida de um número de 01 a 04: \n");
+  scanf("%s", &codigo);
+
+  printf("Digite o nome da cidade: \n");
+  scanf("%s", &cidade);
+
+  printf("Digite o número de habitantes da cidade: \n");
+  scanf("%s", &populacao);
+
+  printf("Digite a área da cidade em quilômetros quadrados: \n");
+  scanf("%s", &area);
+
+  printf("Digite o produto interno bruto da cidade: \n");
+  scanf("%c", &pib);
+
+  printf("Digite o número de pontos turísticos: \n");
+  scanf("%d", &pontos);
+
+
+//Carta 2:
+   printf("Carta 2: \n");
+  printf("Digite uma letra de 'A' a 'Z': \n");
+  scanf("%s", &estado);
+
+  printf("Digite a letra do estado seguida de um número de 01 a 04: \n");
+  scanf("%s", &codigo);
+
+  printf("Digite o nome da cidade: \n");
+  scanf("%s", &cidade);
+
+  printf("Digite o número de habitantes da cidade: \n");
+  scanf("%s", &populacao);
+
+  printf("Digite a área da cidade em quilômetros quadrados: \n");
+  scanf("%s", &area);
+
+  printf("Digite o produto interno bruto da cidade: \n");
+  scanf("%c", &pib);
+
+  printf("Digite o número de pontos turísticos: \n");
+  scanf("%d", &pontos);
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
